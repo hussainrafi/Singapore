2ec5cbd126016c929093064d9599845f9466b0b2
pvburleigh <pvburleigh@gmail.com>
2019-10-18 11:56:18 +0200

    nu med comments

--- User.js
+++ User.js
@@ -8,14 +8,14 @@ class User {
          //this.sportLevel = sportLevel;
      }
 }
-
+//Coach klasse, som er nedarvet fra User
 class Coach extends User{
     constructor(firstName, lastName, username, password, coachID){
         super(firstName, lastName, username, password);
         this.coachID = coachID;
     }
 }
-
+//Student klasse, som er nedarvet fra User
 class Student extends User{
     constructor(firstName, lastName, username, password, studentID){
       super(firstName, lastName, username, password);
@@ -23,7 +23,7 @@ class Student extends User{
     }
 }
 
-//Liste der opbevarer brugerne
+//Tjekker om LocalStorage er tom. Hvis Local storage er tom, bliver de harcodede brugere gemt i local storage.
 if (localStorage.getItem("User") == null) {
     var userList = [];
     //Dummy user data, som objekter der bliver pushet til et empty array
@@ -34,6 +34,7 @@ if (localStorage.getItem("User") == null) {
 
     var userListString = JSON.stringify(userList);
     localStorage.setItem("User", userListString);
+//Hvis LocalStorage ikke er tom, hentes LocalStorage og gemmes i userList
 } else {
     var userList = JSON.parse(localStorage.getItem("User"))
 }
@@ -64,46 +65,52 @@ function login(){
 
 //Opret ny bruger
 function newUser(){
-    //Brugertype importeres (Coach eller Student)
-    var userTypes = document.getElementsByClassName('userType');
-    var len = userTypes.length;
-    var userType = "";
+        //Brugertype importeres (Coach eller Student)
+        var userTypes = document.getElementsByClassName('userType');
+        var len = userTypes.length;
+        var userType = "";
 
-    var i = 0;
-    for (i=0; i<len; i++) {
-        if (userTypes[i].checked) {
-            userType = "Coach"
+        var i = 0;
+        for (i=0; i<len; i++) {
+            if (userTypes[i].checked) {
+                userType = "Coach"
+            } else {
+                userType = "Student"
+            }
+        }
+
+        //Fornavn, efternavn, brugernavn og password importeres
+        var firstName = document.getElementById('newFirstName').value;
+        var lastName = document.getElementById('newLastName').value;
+        var username = document.getElementById('newUsername').value;
+        var password = document.getElementById('newPassword').value;
+
+        //Brugerne i local storage importeres
+        var storedUsersList = JSON.parse(localStorage.getItem("User"));
+
+        if(userType == "Coach"){
+            //Genererer nyt unikt coachID
+            var idNumber = 1;
+            for(i = 0; i< storedUsersList.length; i++) {
+                if (storedUsersList[i].coachID >= 0) {
+                    idNumber += 1
+                }
+            }
+            //Opretter ny coach og gemmer brugeren i local storage
+            userList.push(new Coach(firstName, lastName, username, password, idNumber));
+            var newLocalUserListString = JSON.stringify(userList);
+            localStorage.setItem("User", newLocalUserListString)
         } else {
-            userType = "Student"
-        }
-    }
-
-    var firstName = document.getElementById('newFirstName').value;
-    var lastName = document.getElementById('newLastName').value;
-    var username = document.getElementById('newUsername').value;
-    var password = document.getElementById('newPassword').value;
-
-    var storedUsersList = JSON.parse(localStorage.getItem("User"));
-
-    if(userType == "Coach"){
-        var idNumber = 1;
-        for(i = 0; i< storedUsersList.length; i++) {
-            if (storedUsersList[i].coachID >= 0) {
-                idNumber += 1
+            //Genererer nyt unikt studentID
+            var idNumber = 1;
+            for(i = 0; i< storedUsersList.length; i++) {
+                if (storedUsersList[i].studentID >= 0) {
+                    idNumber += 1
+                }
             }
+            //Opretter ny student og gemmer brugeren i local storage
+            userList.push(new Student(firstName, lastName, username, password, idNumber));
+            var newLocalUserListString = JSON.stringify(userList);
+            localStorage.setItem("User", newLocalUserListString)
         }
-        userList.push(new Coach(firstName, lastName, username, password, idNumber));
-        var newLocalUserListString = JSON.stringify(userList);
-        localStorage.setItem("User", newLocalUserListString)
-    } else {
-        var idNumber = 1;
-        for(i = 0; i< storedUsersList.length; i++) {
-            if (storedUsersList[i].studentID >= 0) {
-                idNumber += 1
-            }
-        }
-        userList.push(new Student(firstName, lastName, username, password, idNumber));
-        var newLocalUserListString = JSON.stringify(userList);
-        localStorage.setItem("User", newLocalUserListString)
-    }
 }
