6b011c82d203026a00421e1f679d25734d83b300
Onur Karakus <onka19ab@student.cbs.dk>
2020-04-06 01:57:18 +0200

    Oprettelse af første table, forsøger at etablerer kontakt til database i morgen!

---
+++ tables.js
@@ -0,0 +1,32 @@
+const pool = require('./db');
+pool.query(`
+CREATE TABLE Coach (
+coachid SERIAL PRIMARY KEY
+userName text NOT NULL,
+password text NOT NULL,
+firstName text NOT NULL,
+lastName text NOT NULL,
+sportsTeam text NOT NULL);
+
+CREATE TABLE Student (
+studentid SERIAL PRIMARY KEY
+userName text NOT NULL,
+password text NOT NULL,
+firstname text NOT NULL,
+lastName text NOT NULL),
+sportsTeam text NOT NULL);
+
+CREATE TABLE Session (
+team text NOT NULL,
+Coach_id INT REFERENCES Coach (id));
+student_id INT REFERENCES Student(id));
+facility_id INT REFERENCES Facility (id)); 
+timeInterval text NOT NULL);
+
+CREATE TABLE Facility (
+facilityID SERIAL PRIMARY KEY
+capacity text NOT NULL,
+suitable text NOT NULL);
+`).then(result => {
+    console.log(error, result);
+});
